ca62c7f97025780cdf7b3ce0cf12c95ec0be43de
manuel.martinez.fdez <manuel.martinez.fdez@064b8cea-ee9b-b3df-7b87-43fdc4e54f5b>
2011-10-21 18:53:58 +0000

    Lo poco que he hecho

--- trunk/bitAjedrez/src/bitajedrez/Jugador.java
+++ trunk/bitAjedrez/src/bitajedrez/Jugador.java
@@ -4,6 +4,29 @@ import java.util.List;
 
 public class Jugador {
     private String nombre;
+    private String color; //Blanca o Negra
     List <Pieza> listPieza;
+
+    public Jugador(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
 }
--- trunk/bitAjedrez/src/bitajedrez/Partida.java
+++ trunk/bitAjedrez/src/bitajedrez/Partida.java
@@ -1,11 +1,30 @@
 package bitajedrez;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Partida {
     List<Jugador> listJugador;
     private Tablero tablero;
+
+    public Partida() {
+        listJugador=new ArrayList();
+    }
     
+    public boolean introducirJugador(Jugador jugador){
+        //Comprobamos si la partida está llena
+        if(listJugador.size()==2){
+            return false;
+        }
+        //Comprobamos que si ya hay un jugador, el otro usa el otro color
+        if(listJugador.size()==1){
+            if(listJugador.get(0).getColor().equals(jugador.getColor())){
+                return false;
+            }
+        }
+        //Introducimos al jugador en la partida
+        return listJugador.add(jugador);
+    }
     
     
     
