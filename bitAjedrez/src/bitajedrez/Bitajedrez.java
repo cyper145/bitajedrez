f98f7cd7c989829c2ec01729edc5798ec714e73b
manuel.martinez.fdez <manuel.martinez.fdez@064b8cea-ee9b-b3df-7b87-43fdc4e54f5b>
2011-10-21 18:53:58 +0000

    Lo poco que he hecho

--- bitAjedrez/src/bitajedrez/Bitajedrez.java
+++ bitAjedrez/src/bitajedrez/Bitajedrez.java
@@ -3,7 +3,18 @@ package bitajedrez;
 public class Bitajedrez {
 
     public static void main(String[] args) {
+        //Creamos la partida
         Partida partida=new Partida();
         
+        //Creamos unos jugadores
+        Jugador jugador1=new Jugador("Manuel", "blanca");
+        Jugador jugador2=new Jugador("Espinete", "negra");
+        
+        //Añadimos los jugadores a la partida
+        partida.introducirJugador(jugador1);
+        partida.introducirJugador(jugador2);
+        
+        //Comienzan las blancas
+        partida.asignarTurno(partida.getJugador("blanca"));
     }
 }
